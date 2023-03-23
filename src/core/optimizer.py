0c1acf7c34ad13473b67ffccb46003b3978edab1
Robert Han <roberthan1125@gmail.com>
2023-03-23 17:39:04 +0900

    💅: polised optimizer & new database

--- src/core/optimizer.py
+++ src/core/optimizer.py
@@ -136,12 +136,29 @@ class OptMetrics:
         tracking_error = np.sqrt(tracking_variance)
         return tracking_error
 
+    @staticmethod
+    def expost_tracking_error(
+        weights: np.ndarray,
+        pri_returns_assets: np.ndarray,
+        pri_returns_bm: np.ndarray,
+    ) -> float:
+        """_summary_
+
+        Args:
+            weights (np.ndarray): _description_
+            pri_returns_assets (np.ndarray): _description_
+            pri_returns_benchmark (np.ndarray): _description_
+
+        Returns:
+            float: _description_
+        """
+        rel_return = np.dot(pri_returns_assets, weights) - pri_returns_bm
+        mean = np.sum(rel_return) / len(rel_return)
+        return np.sum(np.square(rel_return - mean))
+
 
 class Optimizer:
     """portfolio optimizer"""
-
-    __constraints__: Dict = {}
-    __metrics__: OptMetrics = OptMetrics()
 
     def __init__(
         self,
@@ -153,6 +170,16 @@ class Optimizer:
         weights_bm: Optional[pd.Series] = None,
     ) -> None:
         """initialization"""
+        
+        if expected_returns is not None:
+            self.expected_returns = expected_returns
+            self.assets = self.expected_returns.index
+        
+        if covariance_matrix is not None:
+            self.covariance_matrix = covariance_matrix
+            
+        
+        
         self.expected_returns = expected_returns
         self.covariance_matrix = covariance_matrix
         self.prices_assets = prices_assets
@@ -160,38 +187,175 @@ class Optimizer:
         self.prices_bm = prices_bm
         self.weights_bm = weights_bm
         self.num_assets = len(self.covariance_matrix)
+        self.constraints: Dict = {}
+        self.metrics: OptMetrics = OptMetrics()
 
-    def set_min_weights(self, min_weights: float = 0.0) -> None:
+        
+
+
+
+
+
+
+    def set_min_weight(self, min_weight: float = 0.0) -> None:
         """set minimum weights constraint"""
-        self.__constraints__.update(
+        self.constraints.update(
             {
-                "min_weights": {
+                "min_weight": {
                     "type": "ineq",
-                    "fun": lambda w: w - min_weights,
+                    "fun": lambda w: w - min_weight,
                 }
             }
         )
 
-    def set_max_weights(self, max_weights: float = 1.0) -> None:
+    def set_max_weight(self, max_weight: float = 1.0) -> None:
         """set maximum weights constraint"""
-        self.__constraints__.update(
+        self.constraints.update(
             {
-                "max_weights": {
+                "max_weight": {
                     "type": "ineq",
-                    "fun": lambda w: max_weights - w,
+                    "fun": lambda w: max_weight - w,
                 }
             }
         )
 
-    def set_sum_weights(self, sum_weights: float = 1.0) -> None:
+    def set_sum_weight(self, sum_weight: float = 1.0) -> None:
         """set summation weights constriant"""
-        self.__constraints__.update(
+        self.constraints.update(
             {
-                "sum_weights": {
-                    "sum_weights": {
-                        "type": "eq",
-                        "fun": lambda w: np.sum(w) - sum_weights,
-                    }
+                "sum_weight": {
+                    "type": "eq",
+                    "fun": lambda w: np.sum(w) - sum_weight,
                 }
             }
         )
+
+    def set_min_return(self, min_return: float = 0.05) -> None:
+        """set minimum return constraint"""
+        if self.expected_returns is None:
+            warnings.warn("unable to set minimum return constraint.")
+            warnings.warn("expected returns is null.")
+        self.constraints.update(
+            {
+                "min_return": {
+                    "type": "ineq",
+                    "fun": lambda w: self.metrics.expected_return(
+                        weights=w, expected_returns=self.expected_returns.values
+                    )
+                    - min_return,
+                }
+            }
+        )
+
+    def set_max_return(self, max_return: float = 0.05) -> None:
+        """set maximum return constraint"""
+        if self.expected_returns is None:
+            warnings.warn("unable to set maximum return constraint.")
+            warnings.warn("expected returns is null.")
+        self.constraints.update(
+            {
+                "max_return": {
+                    "type": "ineq",
+                    "fun": lambda w: max_return
+                    - self.metrics.expected_return(
+                        weights=w, expected_returns=self.expected_returns.values
+                    ),
+                }
+            }
+        )
+
+    def set_min_volatility(self, min_volatility: float = 0.05) -> None:
+        """set minimum volatility constraint"""
+        if self.expected_returns is None:
+            warnings.warn("unable to set minimum volatility constraint.")
+            warnings.warn("expected returns is null.")
+        self.constraints.update(
+            {
+                "min_volatility": {
+                    "type": "ineq",
+                    "fun": lambda w: self.metrics.expected_volatility(
+                        weights=w, covariance_matrix=self.covariance_matrix.values
+                    )
+                    - min_volatility,
+                }
+            }
+        )
+
+    def set_max_volatility(self, max_volatility: float = 0.05) -> None:
+        """set maximum volatility constraint"""
+        if self.expected_returns is None:
+            warnings.warn("unable to set maximum volatility constraint.")
+            warnings.warn("expected returns is null.")
+        self.constraints.update(
+            {
+                "max_volatility": {
+                    "type": "ineq",
+                    "fun": lambda w: max_volatility
+                    - self.metrics.expected_volatility(
+                        weights=w, covariance_matrix=self.covariance_matrix.values
+                    ),
+                }
+            }
+        )
+
+    def set_max_active_weight(self, max_active_weight: float = 0.10) -> None:
+        """set maximum active weight against benchmark"""
+        if self.weights_bm is None:
+            warnings.warn("unable to set maximum active weight constraint.")
+            warnings.warn("benchmark weights is null.")
+        self.constraints.update(
+            {
+                "max_active_weight": {
+                    "type": "ineq",
+                    "fun": lambda w: max_active_weight
+                    - np.sum(np.abs(w - self.weights_bm.values)),
+                }
+            }
+        )
+
+    def set_max_exante_tracking_error(
+        self, max_exante_tracking_error: float = 0.02
+    ) -> None:
+        """set maximum exante tracking error constraint"""
+
+        self.constraints.update(
+            {
+                "max_exante_tracking_error": {
+                    "type": "ineq",
+                    "fun": lambda w: max_exante_tracking_error
+                    - self.metrics.exante_tracking_error(
+                        weights=w,
+                        weights_bm=self.weights_bm.values,
+                        covariance_matrix=self.covariance_matrix.values,
+                    ),
+                }
+            }
+        )
+
+    def set_max_expost_tracking_error(
+        self, max_expost_tracking_error: float = 0.02
+    ) -> None:
+        """set maximum expost tracking error constraint"""
+
+        itx = self.prices_assets.dropna().index.intersection(
+            self.prices_bm.dropna().index
+        )
+
+        pri_returns_assets = self.prices_assets.loc[itx].pct_change().fillna(0)
+        pri_returns_bm = self.prices_bm.loc[itx].pct_change().fillna(0)
+
+        self.constraints.update(
+            {
+                "max_expost_tracking_error": {
+                    "type": "ineq",
+                    "fun": lambda w: max_expost_tracking_error
+                    - self.metrics.expost_tracking_error(
+                        weights=w,
+                        pri_returns_assets=pri_returns_assets.values,
+                        pri_returns_bm=pri_returns_bm.values,
+                    ),
+                }
+            }
+        )
+
+
--- src/database/models/meta.py
+++ src/database/models/meta.py
@@ -1,29 +1,23 @@
 import sqlalchemy as sa
-from .mixins import StaticBase
+from .mixins import StaticBase, TimeSeriesBase
 
 
 class Meta(StaticBase):
     """table meta"""
 
-    __tablename__ = "tbmeta"
+    __tablename__ = "tb_meta"
 
     id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
-    ticker = sa.Column(sa.VARCHAR(255))
-    name = sa.Column(sa.VARCHAR(255))
+    ticker = sa.Column(sa.VARCHAR(255), nullable=False)
+    name = sa.Column(sa.VARCHAR(255), nullable=False)
     description = sa.Column(sa.VARCHAR(1000), nullable=True)
-
-    __table_args__ = (
-        sa.UniqueConstraint("id"),
-        sa.UniqueConstraint("ticker"),
-        sa.UniqueConstraint("name"),
-    )
-
+    deactive = sa.Column(sa.Boolean, default=False, nullable=False)
 
 class Source(StaticBase):
     """ticker mappings"""
 
-    __tablename__ = "tbsource"
-    meta_id = sa.Column(sa.ForeignKey("tbmeta.id"), primary_key=True)
+    __tablename__ = "tb_source"
+    meta_id = sa.Column(sa.ForeignKey("tb_meta.id"), primary_key=True)
     source = sa.Column(sa.VARCHAR(255), nullable=False, default="NOTSET")
     bloomberg = sa.Column(sa.VARCHAR(255), nullable=True, default=None)
     yahoo = sa.Column(sa.VARCHAR(255), nullable=True, default=None)
@@ -35,8 +29,8 @@ class Source(StaticBase):
 class Strategy(StaticBase):
     """strategy"""
 
-    __tablename__ = "tbstrategy"
-    meta_id = sa.Column(sa.ForeignKey("tbmeta.id"), primary_key=True)
+    __tablename__ = "tb_strategy"
+    meta_id = sa.Column(sa.ForeignKey("tb_meta.id"), primary_key=True)
     name = sa.Column(sa.VARCHAR(255), nullable=False)
     frequency = sa.Column(sa.VARCHAR(255), nullable=False, default="NOTSET")
 
@@ -44,21 +38,37 @@ class Strategy(StaticBase):
 class Equity(StaticBase):
     """strategy"""
 
-    __tablename__ = "tbequity"
-    meta_id = sa.Column(sa.ForeignKey("tbmeta.id"), primary_key=True)
+    __tablename__ = "tb_equity"
+    meta_id = sa.Column(sa.ForeignKey("tb_meta.id"), primary_key=True)
 
 
 class Index(StaticBase):
     """index"""
 
-    __tablename__ = "tbindex"
-    meta_id = sa.Column(sa.ForeignKey("tbmeta.id"), primary_key=True)
+    __tablename__ = "tb_index"
+    meta_id = sa.Column(sa.ForeignKey("tb_meta.id"), primary_key=True)
 
 
 class Universe(StaticBase):
     """universe"""
 
-    __tablename__ = "tbuniverse"
-    strategy_id = sa.Column(sa.ForeignKey("tbstrategy.meta_id"), primary_key=True)
-    meta_id = sa.Column(sa.ForeignKey("tbequity.meta_id"), primary_key=True)
+    __tablename__ = "tb_universe"
+    strategy_id = sa.Column(sa.ForeignKey("tb_strategy.meta_id"), primary_key=True)
+    meta_id = sa.Column(sa.ForeignKey("tb_equity.meta_id"), primary_key=True)
 
+
+class EquityDailyBar(StaticBase):
+    
+    __tablename__ = "tb_equity_daly_bar"
+    meta_id = sa.Column(sa.ForeignKey("tb_equity.meta_id"), primary_key=True)
+    date = sa.Column(sa.Date, primary_key=True, nullable=False, index=True)
+    open = sa.Column(sa.Numeric(20, 5), nullable=True)
+    high = sa.Column(sa.Numeric(20, 5), nullable=True)
+    low = sa.Column(sa.Numeric(20, 5), nullable=True)
+    close = sa.Column(sa.Numeric(20, 5), nullable=True)
+    volume = sa.Column(sa.Numeric(20, 5), nullable=True)
+    dvds = sa.Column(sa.Numeric(20, 5), nullable=True)
+    splits = sa.Column(sa.Numeric(20, 5), nullable=True)
+    tot_return = sa.Column(sa.Numeric(20, 10), nullable=True)
+    
+    
